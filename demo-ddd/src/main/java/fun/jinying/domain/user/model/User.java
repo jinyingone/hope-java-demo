b96b904d57a73e998e4771178aae7a8cc9c1e3ab
jinying <jinyinshi@sohu-inc.com>
2020-04-21 23:38:23 +0800

    重构user

--- demo-ddd/src/main/java/fun/jinying/domain/user/model/User.java
+++ demo-ddd/src/main/java/fun/jinying/domain/user/model/User.java
@@ -1,19 +1,20 @@
 package fun.jinying.domain.user.model;
 
-import fun.jinying.domain.shard.model.DomainEvent;
 import fun.jinying.domain.shard.model.Entity;
-import lombok.Data;
+import lombok.*;
 
 import java.util.Collections;
 import java.util.Date;
-import java.util.UUID;
 
 /**
  * @description: 用户
  * @author: sjy
  * @create: 2020-02-26 22:01
  **/
-@Data
+@Getter
+@Setter(AccessLevel.PRIVATE)
+@ToString
+@EqualsAndHashCode(of = {"userId"})
 public class User implements Entity {
     private Integer userId;
     private String userName;
@@ -23,7 +24,7 @@ public class User implements Entity {
     private Date updateTime;
 
     /**
-     * 创建一个新用户
+     * 创建一个新用户，这是一个工厂方法
      * @param userId userid
      * @param phone 手机号
      * @param userName 名称
@@ -42,6 +43,10 @@ public class User implements Entity {
         return user;
     }
 
+    /**
+     * 注册
+     * @return 注册事件
+     */
     public UserEvent register(){
         UserRegisteredEvent userRegisteredEvent = new UserRegisteredEvent();
         userRegisteredEvent.setUserId(this.userId.toString());
@@ -49,6 +54,10 @@ public class User implements Entity {
         return userRegisteredEvent;
     }
 
+    /**
+     * 登陆
+     * @return 登陆事件
+     */
     public UserEvent login() {
         UserLoggedEvent event = new UserLoggedEvent();
         event.setUserId(this.getUserId().toString());
@@ -56,6 +65,11 @@ public class User implements Entity {
         return event;
     }
 
+    /**
+     * 更新用户名
+     * @param userName 新的用户名
+     * @return 更新事件
+     */
     public UserUpdatedEvent updateUserName(String userName){
         this.userName = userName;
 
@@ -64,16 +78,6 @@ public class User implements Entity {
         userUpdatedEvent.setUserId(this.getUserId().toString());
         userUpdatedEvent.setUpdatedFields(Collections.singletonMap("userName",userName));
         return userUpdatedEvent ;
-    }
-
-    /**
-     * 更改用户信息
-     *
-     * @param user
-     * @return
-     */
-    public User updateUser(User user) {
-        return user;
     }
 
 }
