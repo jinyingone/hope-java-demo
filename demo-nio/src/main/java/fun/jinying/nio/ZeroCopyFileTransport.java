cfebf5a913fd326a2e9afea927fa4875c661cb22
jy <jinyingshi@sohu-inc.com>
2020-04-28 20:51:09 +0800

    零拷贝

--- demo-nio/src/main/java/fun/jinying/nio/ZeroCopyFileTransport.java
+++ demo-nio/src/main/java/fun/jinying/nio/ZeroCopyFileTransport.java
@@ -2,6 +2,7 @@ package fun.jinying.nio;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.channels.FileChannel;
 
@@ -14,7 +15,7 @@ import java.nio.channels.FileChannel;
 public class ZeroCopyFileTransport {
     public void copy(File src, File dst) throws IOException {
         try (FileChannel srcChannel = new FileInputStream(src).getChannel()) {
-            try (FileChannel dstChannel = new FileInputStream(dst).getChannel()) {
+            try (FileChannel dstChannel = new FileOutputStream(dst).getChannel()) {
                 srcChannel.transferTo(0,src.length(),dstChannel);
             }
         }
